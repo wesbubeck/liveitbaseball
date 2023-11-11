b101e27709cde406e96856094dde589638d3bc9a
wesbubeck <wesbubeck@gmail.com>
2023-11-10 21:43:40 -0500

    init

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -5,18 +5,17 @@ export function Welcome() {
   return (
     <>
       <Title className={classes.title} ta="center" mt={100}>
-        Welcome to{' '}
-        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+        {/* Welcome to{' '} */}
+        <Text inherit variant="gradient" component="span" gradient={{ from: 'blue', to: 'green' }}>
+          Live It Baseball
         </Text>
       </Title>
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Next.js project includes a minimal setup for server side rendering, if you want
-        to learn more on Mantine + Next.js integration follow{' '}
-        <Anchor href="https://mantine.dev/guides/next/" size="lg">
+        Formally known as ripping tits
+        {/* <Anchor href="https://mantine.dev/guides/next/" size="lg">
           this guide
         </Anchor>
-        . To get started edit page.tsx file.
+        . To get started edit page.tsx file. */}
       </Text>
     </>
   );
